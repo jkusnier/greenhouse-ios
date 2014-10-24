41b98b87e0cac767df1ad91b30e104a32f8d2f1c
Jason Kusnier <jkusnier@gmail.com>
2014-10-23 21:10:40 -0400

    basic skeleton app
    shows current temp and refreshes only when app is visible

--- greenhouse-ios/ViewController.swift
+++ greenhouse-ios/ViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var lastUpdatedLabel: UILabel!
+    
+    let url:NSURL = NSURL(string: "http://api.weecode.com/greenhouse/v1/devices/50ff6c065067545628550887/environment")!
+    var mainTimer:NSTimer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mainTimer = NSTimer.scheduledTimerWithTimeInterval(60, target: self, selector: Selector("updateTitle"), userInfo: nil, repeats: true)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "resetTimer:", name: NSSystemClockDidChangeNotification, object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "stopTimer", name: UIApplicationWillResignActiveNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "resetTimer:", name: UIApplicationWillEnterForegroundNotification, object: nil)
+        
+        updateTitle()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func resetTimer(aNotification: NSNotification) {
+        mainTimer?.invalidate()
+        mainTimer = NSTimer.scheduledTimerWithTimeInterval(60, target: self, selector: Selector("updateTitle"), userInfo: nil, repeats: true)
+        updateTitle()
+    }
+    
+    func stopTimer() {
+        mainTimer?.invalidate()
     }
 
-
+    func updateTitle() {
+        var tempString = "--°"
+        var error: NSError?
+        let jsonData = NSData(contentsOfURL: url)
+        if (jsonData != nil) {
+            let jsonDict = NSJSONSerialization.JSONObjectWithData(jsonData!, options: nil, error: &error) as NSDictionary
+            
+            
+            if (error == nil) {
+                tempString = String(format: "%.1f°", jsonDict["fahrenheit"] as Double)
+                lastUpdatedLabel.text = NSDate(dateString: jsonDict["published_at"] as String).localFormat()
+            }
+        }
+        temperatureLabel.text = tempString
+    }
 }
 
