7f9b39545e136fba4dc1909dbe047887fb6eb20c
Jason Kusnier <jkusnier@gmail.com>
2015-04-02 20:13:36 -0400

    class added to hold api calls

---
+++ GreenhouseData/GreenhouseAPI.swift
@@ -0,0 +1,13 @@
+//
+//  GreenhouseAPI.swift
+//  Wee Greenhouse Monitor
+//
+//  Created by Jason Kusnier on 4/2/15.
+//  Copyright (c) 2015 Jason Kusnier. All rights reserved.
+//
+
+import Foundation
+
+public class GreenhouseAPI {
+    
+}
