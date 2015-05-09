16663c71f0487057a83ed58293022ed2d2989537
Jason Kusnier <jkusnier@gmail.com>
2015-05-08 20:10:19 -0400

    updates on view activation

--- Wee Greenhouse Monitor WatchKit Extension/InterfaceController.swift
+++ Wee Greenhouse Monitor WatchKit Extension/InterfaceController.swift
@@ -66,6 +66,8 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        updateTitle()
     }
 
     override func didDeactivate() {
